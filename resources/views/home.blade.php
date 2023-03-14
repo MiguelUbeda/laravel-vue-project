ca5ab6ddaa788949a8ed66ccc9ac83e3110aba29
Miguel Ubeda <miguelubeda09@hotmail.com>
2023-03-14 13:38:13 +0100

    finished bootstrap and username added to db

---
+++ resources/views/home.blade.php
@@ -0,0 +1,35 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row  justify-content-center d-flex" >
+        <div class="col-4 pt-5" >
+            <div class="text-end">
+                <img src="\jpg\profile.jpg" alt="" class="rounded-circle ms-5" style="height: 125px;">
+            </div>
+        </div>
+        <div class="col-8 pt-5">
+            <div><h1>NPC User</h1></div>
+            <div class="d-flex">
+                <div class="pe-4"><strong>150</strong> posts</div>
+                <div class="pe-4"><strong>10k</strong> followers</div>
+                <div class="pe-4"><strong>200</strong> following</div>
+            </div>
+            <div class="pt-4"><strong>Npcgram.org</strong></div>
+            <div>Full stack developer developing Laravel app</div>
+            <div><a href="#">www.mautgram.org</a></div>
+        </div>
+    </div>
+    <div class="row pt-5">
+        <div class="col-4">
+            <img src="https://cdn.pixabay.com/photo/2014/02/27/16/10/flowers-276014__340.jpg" alt="" class="w-100" style="height: 250px;">
+        </div>
+        <div class="col-4">
+            <img src="https://cdn.pixabay.com/photo/2015/06/19/21/24/avenue-815297__340.jpg" alt="" class="w-100" style="height: 250px;">
+        </div>
+        <div class="col-4">
+            <img src="https://t4.ftcdn.net/jpg/02/91/24/27/360_F_291242770_z3XC7rJB1Mvc5jVMsEY9Dx2xMrX4sxUi.jpg" alt="" class="w-100" style="height: 250px;">
+        </div>
+    </div>
+</div>
+@endsection
